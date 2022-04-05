56a9fc771ede2626766582df988671fc3b2e9d6d
rajeshmn47 <rajeshmn47@gmail.com>
2022-04-05 19:46:13 +0530

    last commit

--- src/components/search/Tearch.js
+++ src/components/search/Tearch.js
@@ -8,7 +8,9 @@ export const Searc=()=>{
         <div className='search'>
 <TextField style={{width:'100%',height:'5vh',padding:'1vmax'}} variant='outlined' placeholder='search for item or a store'/>
 <Search style={{position:'absolute',top:'50%',right:'10%'}}/>
-<CircularProgress htmlcolor='#00D290'/>
+<div style={{height:'100vh',width:'100vw',display:'flex',justifyContent:'center',alignItems:'center'}}>
+<CircularProgress style={{color:'#00D290'}}/>
+</div>
 </div>
         </>
     )
