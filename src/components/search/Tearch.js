7a92b709d1402d7532accecedae6b3e6066d94d6
rajeshmn47 <rajeshmn47@gmail.com>
2022-04-05 19:04:36 +0530

    last commit

--- src/components/search/Tearch.js
+++ src/components/search/Tearch.js
@@ -1,5 +1,6 @@
-import { TextField } from "@material-ui/core"
+import { TextField,CircularProgress } from "@material-ui/core"
 import { Search } from "@material-ui/icons"
+
 
 export const Searc=()=>{
     return(
@@ -7,6 +8,7 @@ export const Searc=()=>{
         <div className='search'>
 <TextField style={{width:'100%',height:'5vh',padding:'1vmax'}} variant='outlined' placeholder='search for item or a store'/>
 <Search style={{position:'absolute',top:'50%',right:'10%'}}/>
+<CircularProgress htmlColor='#00D290'/>
 </div>
         </>
     )
