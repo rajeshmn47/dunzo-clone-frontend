2db3ff86ebd3975ee2fe53a38ee90b09d73d695f
rajeshmn47 <rajeshmn47@gmail.com>
2022-04-06 09:34:15 +0530

    keyboard

--- src/components/user/profile.js
+++ src/components/user/profile.js
@@ -1,8 +1,13 @@
+import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace';
+
 export const Profile=()=>{
     return(
         <>
+        <div style={{display:'flex',alignItems:'center'}}>
+        <KeyboardBackspaceIcon/>
         <img src='https://resources.dunzo.com/web-assets/prod/_next/…/ima
         ges/logo-7b2b540792556466133aea6e7c6ae513.png' alt='' width='40'/>
+        </div>
         </>
     )
 }
