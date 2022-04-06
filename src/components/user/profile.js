525dc1f711fc3b72e050c37a2c46fc83782958d3
rajeshmn47 <rajeshmn47@gmail.com>
2022-04-06 10:01:41 +0530

    keyboard

--- src/components/user/profile.js
+++ src/components/user/profile.js
@@ -5,8 +5,8 @@ export const Profile=()=>{
         <>
         <div style={{display:'flex',alignItems:'center'}}>
         <KeyboardBackspaceIcon/>
-        <img src='https://resources.dunzo.com/web-assets/prod/_next/…/ima
-        ges/logo-7b2b540792556466133aea6e7c6ae513.png' alt='' width='40'/>
+        <img src='https://resources.dunzo.com/web-assets/prod/_next/static/images/logo-7b2b540792556466133aea6e7c6ae513.png' 
+        alt='' height='14' width='61'/>
         </div>
         </>
     )
