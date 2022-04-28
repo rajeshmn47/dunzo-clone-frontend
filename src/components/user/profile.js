5249eb3165ac37ed47205c9761ddd20fbe775178
rajeshmn47 <rajeshmn47@gmail.com>
2022-04-28 23:12:23 +0530

    last commit

--- src/components/user/profile.js
+++ src/components/user/profile.js
@@ -1,5 +1,8 @@
 import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace';
 import {auth} from '../../firebase'
+import ListOutlinedIcon from '@material-ui/icons/ListOutlined';
+import ChevronRightOutlinedIcon from '@material-ui/icons/ChevronRightOutlined';
+import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
 
 export const Profile=()=>{
     return(
@@ -9,7 +12,12 @@ export const Profile=()=>{
         <img src='https://resources.dunzo.com/web-assets/prod/_next/static/images/logo-7b2b540792556466133aea6e7c6ae513.png' 
         alt='' height='14' width='61'/>
         </div>
-    <button onClick={()=>auth.signOut()} className='logoutbtn'>Logout</button>
+        <div style={{padding:'1vh 1vw',margin:'1vh 1vw',display:'flex',justifyContent:'spaceBetween',alignItems:'center'}}>
+    <button onClick={()=>auth.signOut()} className='logoutbtn'>
+     <ExitToAppOutlinedIcon/>   
+        Logout</button>
+<ChevronRightOutlinedIcon/>
+    </div>
         </>
     )
 }
