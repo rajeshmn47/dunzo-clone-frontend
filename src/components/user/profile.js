9d62c9b54fc575204a42d4af135ab4636dbf2a59
rajeshmn47 <rajeshmn47@gmail.com>
2022-04-28 23:42:36 +0530

    last commit

--- src/components/user/profile.js
+++ src/components/user/profile.js
@@ -7,12 +7,15 @@ import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
 export const Profile=()=>{
     return(
         <>
-        <div style={{display:'flex',alignItems:'center',padding:'1vw',boxShadow:'rgb(0 0 0 / 6%) 0px 4px 7px'}}>
+        <div style={{display:'flex',alignItems:'center',padding:'1vw',}}>
         <KeyboardBackspaceIcon/>
         <img src='https://resources.dunzo.com/web-assets/prod/_next/static/images/logo-7b2b540792556466133aea6e7c6ae513.png' 
         alt='' height='14' width='61'/>
         </div>
-        <div style={{padding:'1vh 1vw',margin:'1vh 1vw',display:'flex',justifyContent:'spaceBetween',alignItems:'center'}}>
+        <div className='aadhaar'>
+            Verify your age to order paan items
+            </div>
+        <div style={{padding:'1vh 1vw',margin:'1vh 1vw',display:'flex',justifyContent:'space-between',alignItems:'center'}}>
     <button onClick={()=>auth.signOut()} className='logoutbtn'>
      <ExitToAppOutlinedIcon/>   
         Logout</button>
