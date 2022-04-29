a6e18aa50d7543c596ca4d61df052b7c88efe46a
rajeshmn47 <rajeshmn47@gmail.com>
2022-04-30 05:01:23 +0530

    make it better

--- src/components/search/Tearch.js
+++ src/components/search/Tearch.js
@@ -4,6 +4,7 @@ import { useState,useEffect } from "react"
 import axios from 'axios'
 import {useNavigate} from 'react-router-dom'
 import Minicart from './minicart'
+import './storedetails.css'
 
 export const Searc=()=>{
     const[change,setChange]=useState(false)
